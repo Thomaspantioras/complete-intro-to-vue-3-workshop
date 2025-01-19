b6f8b0c3efec549f8d25cc739092966d860dd219
thomaspantioras <t.pantioras@gmail.com>
2025-01-20 00:33:53 +0200

    commit

---
+++ atla-forum/src/Components/BaseCharacters.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>Characters</h2>
+  <p v-if="characterList.length === 0">There are no characters</p>
+  <ul v-else-if="characterList.length % 2 === 0">
+    <li
+      v-for="(character, index) in characterList"
+      :key="`even-character-${index}`"
+    >
+      <p>{{ character.name }}</p>
+      <button @click="favoriteCharacter(character)">⭐ Favorite</button>
+    </li>
+  </ul>
+  <p v-else>There are odd characters!</p>
+</template>
+
+<script>
+export default {
+  props: ["characterList", "favoriteList"],
+  data() {
+    return {};
+  },
+  methods: {
+    favoriteCharacter(character) {
+      this.favoriteList.push(character);
+    },
+  },
+};
+</script>
