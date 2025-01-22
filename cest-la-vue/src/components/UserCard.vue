bdb4d094123a967d70e83491fb09041eb468dd64
thomaspantioras <t.pantioras@gmail.com>
2025-01-22 22:29:53 +0200

    fetch ecxersise

---
+++ cest-la-vue/src/components/UserCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <li class="user-card">{{ user.name }}: {{ user.website }}</li>
+</template>
+
+<style>
+.user-card {
+  border: 1px solid black;
+  padding: 4px;
+}
+</style>
---
+++ cest-la-vue/src/components/UserPage.vue
@@ -0,0 +1,76 @@
+<script>
+// import { v4 as uuidv4 } from 'uuid';
+import UserCard from "./UserCard.vue";
+export default {
+  components: {
+    UserCard,
+  },
+  data: () => ({
+    usersList: [],
+    newUser: {
+      name: "Ben",
+    },
+  }),
+  methods: {
+    // addNewUser() {
+    //   this.usersList.push({
+    //     ...this.newUser,
+    //     id: uuidv4()
+    //   })
+    // },
+    async fetchUsers() {
+      this.usersList = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      ).then((response) => response.json());
+    },
+  },
+  created() {
+    this.fetchUsers();
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h1>Users</h1>
+    <ul>
+      <UserCard
+        v-for="user in usersList"
+        :key="`user-${user.id}`"
+        :user="user"
+      />
+    </ul>
+  </main>
+</template>
+
+<style>
+main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  max-width: 320px;
+  margin: 0 auto;
+}
+
+main h1 {
+  margin-top: 10vh;
+  margin-bottom: 20px;
+}
+
+label {
+  margin-bottom: 5px;
+}
+
+input[type="email"] {
+  padding: 0.5rem;
+  margin-bottom: 30px;
+}
+
+button {
+  border: 1px solid green;
+  padding: 10px;
+  color: green;
+  background-color: rgb(213, 255, 213);
+  cursor: pointer;
+}
+</style>
